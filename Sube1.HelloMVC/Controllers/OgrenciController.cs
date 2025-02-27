6ca3405d09dce0bc855578e5eb50dc2329a49d03
Gazi <Gazi@DESKTOP-224SE1B>
2025-02-27 11:55:42 +0300

    Veri Taşıma Yöntemleri

--- Sube1.HelloMVC/Controllers/OgrenciController.cs
+++ Sube1.HelloMVC/Controllers/OgrenciController.cs
@@ -1,30 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Sube1.HelloMVC.Models;
+using Sube1.HelloMVC.Models.ViewModels;
 
 namespace Sube1.HelloMVC.Controllers
 {
     public class OgrenciController : Controller
     {
         public ViewResult Index()//Action
-        {
-            return View();//TODO:Farklı isimli View örneği
+        {     
+            return View("AnaSayfa");
         }
 
-        public string OgrenciDetay(int ogrenciid)
+        public ViewResult OgrenciDetay()
         {
-            string ogr;
-            if (ogrenciid == 1)
-            {
-                ogr = "Ali Veli ";
-            }
-            else if (ogrenciid == 2)
-            {
-                ogr = "Ahmet Mehmet ";
-            }
-            else
-            {
-                ogr = "Öğrenci Bulunamadı";
-            }
-            return ogr;
+            //Ogrenci ogr = null;
+            //if (id == 1)
+            //{
+            //    ogr = new Ogrenci { Ogrenciid = 1, Ad = "Ali", Soyad = "Veli" };
+            //}
+            //else if (id == 2)
+            //{
+            //    ogr = new Ogrenci { Ogrenciid = 2, Ad = "Ahmet", Soyad = "Mehmet" };
+            //}           
+            //ViewData["ogrenci"] = ogr;
+            //ViewBag.student = ogr;
+            var ogr= new Ogrenci { Ogrenciid = 1, Ad = "Ali", Soyad = "Veli" };
+            var ogretmen = new Ogretmen { Ogretmenid = 1, Ad = "Cihan", Soyad = "Yetişken" };
+
+            var dto = new OgrenciDetayDTO { Ogrenci = ogr, Ogretmen = ogretmen };
+
+            return View(dto);
         }
     }
 }
@@ -32,3 +37,17 @@ namespace Sube1.HelloMVC.Controllers
 //QueryString: Action'ın aldığı parametre id'den farklıysa ? kullanılarak parametreye değer gönderilebilir.
 
 //wwwroot:Web uygalamasının kök dizinidir ve uygulamada kullanılan static dosyalar bu klasörde bulunur.
+
+//Controller->View Veri Taşıma
+//1-ViewData: Dictionary(Collection) Key-Value Pair
+//Key: Tekil olmalıdır ve string veri tipindedir.
+//Value: Object veri tipindedir
+
+//2-ViewBag: Dynamic bir yapıdır. Dynamic yapılar içinde tutulan değerlerin veri tipleri Runtime Sırasında belirlenir.
+//ViewBag verileri saklamak için ViewData koleksiyonunu kullanır. Key-Value.
+//3-Model
+//4-TempData
+
+//ViewModel: Bir View'e birden fazla model taşımak için kullanılır.
+//DTO: Data Transfer Object
+
